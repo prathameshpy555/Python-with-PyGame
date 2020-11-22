6e5820111c49eb19fe11a1f86d8d5b65e05ac14e
Prathamesh Chavan <69964243+prathameshpy555@users.noreply.github.com>
2020-11-22 19:58:49 +0530

    Update Camera.py

--- New/Camera.py
+++ New/Camera.py
@@ -3,8 +3,4 @@ Basic image capturing and display using pygame.camera
 Keyboard controls
 -----------------
 - 0, start camera 0.
-- 1, start camera -1.
-- 0, start camera 0.
-- 0, start camera 0.
-- 0, start camera 0.
-- 0, start camera 0.
+- 1, start camera
